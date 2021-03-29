49c5d383570903f8229728fbca73eff4563dd00a
Manuel Desole <desole.manuel@gmail.com>
2021-03-29 17:22:32 +0200

    changes

--- src/server.js
+++ src/server.js
@@ -31,7 +31,7 @@ server.use(
     // optional, useful for custom headers
     handlePreflightRequest: (req, res) => {
       res.writeHead(200, {
-        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Origin": "https://new-client.vercel.app",
         "Access-Control-Allow-Methods": "GET,POST",
         "Access-Control-Allow-Headers": "my-custom-header",
         "Access-Control-Allow-Credentials": true,
