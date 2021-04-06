9c4084ea12ae77a2220d932ad120295f8ef4eb4a
Manuel Desole <desole.manuel@gmail.com>
2021-04-06 12:36:55 +0200

    check

--- src/Routes/auth/index.js
+++ src/Routes/auth/index.js
@@ -1,19 +1,19 @@
 const authRoutes = require("express").Router();
-const validate = require("../../lib/validation/validationMiddleware");
-const loginSchema = require("../../lib/validation/validationSchema")
-  .loginSchema;
-const passport = require("passport");
-const {
-  loginController,
-  logoutController,
-  refreshTokenController,
-} = require("../../Controllers/authController");
+// const validate = require("../../lib/validation/validationMiddleware");
+// const loginSchema = require("../../lib/validation/validationSchema")
+//   .loginSchema;
+// const passport = require("passport");
+// const {
+//   loginController,
+//   logoutController,
+//   refreshTokenController,
+// } = require("../../Controllers/authController");
 
-const { FRONT_URI } = process.env;
+// const { FRONT_URI } = process.env;
 
-const { generateCookies } = require("../../lib/auth/cookies");
+// const { generateCookies } = require("../../lib/auth/cookies");
 
-authRoutes.post("/login", validate(loginSchema), loginController);
+// authRoutes.post("/login", validate(loginSchema), loginController);
 // authRoutes.post("/refresh", refreshTokenController);
 // authRoutes.post("/logout", logoutController);
 
