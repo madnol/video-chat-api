1fa0e584fc8d2134c8bd69c7c7146fe0bade0454
Manuel Desole <desole.manuel@gmail.com>
2021-04-06 20:35:13 +0200

    uff

--- src/Lib/validation/validationSchema.js
+++ src/Lib/validation/validationSchema.js
@@ -1,17 +1,17 @@
 const Joi = require("joi");
 
 const schemas = {
-  userSchema: Joi.object().keys({
-    name: Joi.string(),
-    lastname: Joi.string(),
-    username: Joi.string(),
-    email: Joi.string().email(),
-    password: Joi.string().min(6),
-  }),
-  loginSchema: Joi.object().keys({
-    username: Joi.string().required(),
-    password: Joi.string().min(6).required(),
-  }),
+  // userSchema: Joi.object().keys({
+  //   name: Joi.string(),
+  //   lastname: Joi.string(),
+  //   username: Joi.string(),
+  //   email: Joi.string().email(),
+  //   password: Joi.string().min(6),
+  // }),
+  // loginSchema: Joi.object().keys({
+  //   username: Joi.string().required(),
+  //   password: Joi.string().min(6).required(),
+  // }),
 };
 
 module.exports = schemas;
